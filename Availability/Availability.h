526ea55782bef34b5c3a910fc617f7e8465fe684
Reggian <1903894+reggian@users.noreply.github.com>
2019-11-12 22:19:54 +0100

    Add Availability framework target.

---
+++ Availability/Availability.h
@@ -0,0 +1,19 @@
+//
+//  Availability.h
+//  Availability
+//
+//  Created by Andreas de Reggi on 12/11/2019.
+//  Copyright © 2019 Reggian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Availability.
+FOUNDATION_EXPORT double AvailabilityVersionNumber;
+
+//! Project version string for Availability.
+FOUNDATION_EXPORT const unsigned char AvailabilityVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Availability/PublicHeader.h>
+
+
