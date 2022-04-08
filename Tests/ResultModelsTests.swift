501172e50cf7a38e3c1fa4828c37991a68f77eaa
reggian <1903894+reggian@users.noreply.github.com>
2022-04-09 00:17:53 +0200

    Add result models and encoding.

---
+++ Tests/ResultModelsTests.swift
@@ -0,0 +1,71 @@
+//
+// ResultModelsTests.swift
+// Availability
+//
+// MIT License
+//
+// Copyright (c) 2022 reggian
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+import XCTest
+@testable import Availability
+
+class ResultModelsTests: XCTestCase {
+  func test_encoding() throws {
+    let result = DeviceInfo(
+      model: "iPhone11,2",
+      name: "iPhone XS",
+      modules: [
+        ModuleInfo(
+          name: "CoreMotion",
+          components: [
+            ComponentInfo(
+              name: "CMSensorRecorder",
+              availability: [
+                "isAccelerometerRecordingAvailable": false
+              ]
+            )
+          ]
+        )
+      ]
+    )
+    
+    let encoder = JSONEncoder()
+    encoder.outputFormatting = .prettyPrinted
+    let jsonData = try encoder.encode(result)
+    let jsonString = try XCTUnwrap(String(data: jsonData, encoding: .utf8))
+    XCTAssertEqual(jsonString,
+      """
+      {
+        "model" : "iPhone11,2",
+        "name" : "iPhone XS",
+        "availability" : {
+          "CoreMotion" : {
+            "CMSensorRecorder" : {
+              "isAccelerometerRecordingAvailable" : false
+            }
+          }
+        }
+      }
+      """
+    )
+  }
+}
